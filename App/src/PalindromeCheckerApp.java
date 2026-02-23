9a2015ebea70b8724e107502acc94ef59f3fbdf4
Puneet Kh <kh16puneet@gmail.com>
2026-02-23 15:47:31 +0530

    Puneet Added Usecase5

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,29 +1,22 @@
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
     public static void main(String[] args){
         System.out.println("Welcome to my Palindrome Checker Management System \nVersion : 1.0 \nSystem Initialized Successfully\n");
 
         String str = "racecar";
-        int len = str.length();
-        boolean ans = true;
-        char arr[] = new char[len];
+        String rev="";
+        Stack <Character> sta = new Stack<>();
+            for (int i=0; i<str.length(); i++)
+                sta.push(str.charAt(i));
 
-        for (int i=0; i<len; i++){
-            arr[i] = str.charAt(i);
-        }
+            for (int i=0; i<str.length(); i++)
+                rev += sta.pop();
 
-        int j=0, k=len-1;
-        while(j<=k){
-            if (arr[j] != arr[k]){
-                ans = false;
-                break;
-            }
-            j++;
-            k--;
-        }
+            if (str.equals(rev))
+                System.out.println("Palindrome");
+            else
+                System.out.println("Not a Palindrome");
 
-        if (ans)
-            System.out.println("Your string is a palindrome");
-        else
-            System.out.println("String is not a palindrome");
     }
 }
