863f232eca33804e8899add2e825b206f86f2ddf
Puneet Kh <kh16puneet@gmail.com>
2026-02-23 15:55:45 +0530

    Puneet Added Usecase6

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,22 +1,30 @@
+import java.util.LinkedList;
 import java.util.Stack;
+import java.util.Queue;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args){
         System.out.println("Welcome to my Palindrome Checker Management System \nVersion : 1.0 \nSystem Initialized Successfully\n");
 
         String str = "racecar";
-        String rev="";
+        boolean ans = true;
         Stack <Character> sta = new Stack<>();
+        Queue <Character> que = new LinkedList<>();
+
             for (int i=0; i<str.length(); i++)
                 sta.push(str.charAt(i));
-
             for (int i=0; i<str.length(); i++)
-                rev += sta.pop();
+                que.add(str.charAt(i));
 
-            if (str.equals(rev))
+            for (int i=0; i<str.length(); i++){
+                if (que.remove() != sta.pop()){
+                    ans = false;
+                    break;
+                }
+            }
+            if (ans)
                 System.out.println("Palindrome");
             else
                 System.out.println("Not a Palindrome");
-
     }
 }
